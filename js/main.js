9947b24b0902d41d59dbb93e1d921692e0ab3f87
melodoc <liubov_rz@yahoo.com>
2021-07-13 21:08:51 +0300

    Move constants to upper scope

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function getRandomPositiveInteger(first, second) {
 }
 
 
-function getRandomPositiveFloat (first, second, digits = 1) {
+function getRandomPositiveFloat(first, second, digits = 1) {
   const lower = Math.min(Math.abs(first), Math.abs(second));
   const upper = Math.max(Math.abs(first), Math.abs(second));
   const result = Math.random() * (upper - lower) + lower;
@@ -19,6 +19,40 @@ function getRandomPositiveFloat (first, second, digits = 1) {
 const FILE_PATH = 'img/avatars/user';
 const FILE_EXTENSION = '.png';
 const EXTREME_TWO_DIGIT_NUMBER = 99;
+const ADJECTIVES = ['Lovely', 'Beautiful', 'Huge', 'Family', 'Youth', 'Conceptual'];
+const NOUNS = ['Abby apartment', 'Milano Flat', 'Guest-house', 'Hilton bungalow'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const DESCRIPTION = ['This town is called: Norubburg',
+  'It was built: in the west',
+  'Traditional city food: fish',
+  'The given temperature in the city: + 26 ℃',
+  'It was founded: 2600 years ago',
+  'Date of foundation by days and months: February 2nd',
+  'Feature of the city: the richest people live in this city',
+  'The area is: 1400 km²',
+  'Population: 360,000 inhabitants',
+  'Number of buildings: 310.000',
+  'The mayors name is: Radmir Lvovich Shchedrin',
+  'The attraction of the city is: first colorful area and first huge park'
+];
+
+const TIME = ['12:00', '13:00', '14:00'];
+const LAT_RANGE = {
+  min: 35.65000,
+  max: 35.70000,
+};
+
+const LNG_RANGE = {
+  min: 139.70000,
+  max: 139.80000,
+};
+
 
 class AdDataBuilder {
   constructor(similarAdDescription = [], NUMBER_OF_ADS_NEARBY = 10) {
@@ -34,8 +68,6 @@ class AdDataBuilder {
         locaion: this.getLocaionsValue(),
       });
     }
-
-
     return this.similarAdDescription;
   }
 
@@ -79,12 +111,9 @@ class AdDataBuilder {
    * Generates ad title
    */
   getAdTitle() {
-    const adjectives = ['Lovely', 'Beautiful', 'Huge', 'Family', 'Youth', 'Conceptual'];
-    const nouns = ['Abby apartment', 'Milano Flat', 'Guest-house', 'Hilton bungalow'];
-
-    const randomAdjectives = this.getRandomIndex(adjectives);
-    const randomNoun = this.getRandomIndex(nouns);
-    const name = `${adjectives[randomAdjectives]  } ${  nouns[randomNoun]}`;
+    const randomAdjectives = this.getRandomIndex(ADJECTIVES);
+    const randomNoun = this.getRandomIndex(NOUNS);
+    const name = `${ADJECTIVES[randomAdjectives]  } ${  NOUNS[randomNoun]}`;
     return name;
   }
 
@@ -92,9 +121,7 @@ class AdDataBuilder {
    * Generates ad type
    */
   setAdType() {
-    const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-    return this.getRandomArrElem(types);
-
+    return this.getRandomArrElem(TYPES);
   }
 
 
@@ -102,54 +129,35 @@ class AdDataBuilder {
    * Generates ad features
    */
   getAdFeatures() {
-    const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-    return [...new Set(this.getRandomArr(features, getRandomPositiveInteger(1, features.length - 1)))];
+    return [...new Set(this.getRandomArr(FEATURES, getRandomPositiveInteger(1, FEATURES.length - 1)))];
   }
 
   /** *
    * Generates ad photos
    */
   getAdPhotos() {
-    const photos = [ 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-
-    return this.getRandomArr(photos, getRandomPositiveInteger(1, 100));
+    return this.getRandomArr(PHOTOS, getRandomPositiveInteger(1, 100));
   }
 
   /** *
    * Generates ad description
    */
   getAdDescription() {
-    const description = [ 'This town is called: Norubburg',
-      'It was built: in the west',
-      'Traditional city food: fish',
-      'The given temperature in the city: + 26 ℃',
-      'It was founded: 2600 years ago',
-      'Date of foundation by days and months: February 2nd',
-      'Feature of the city: the richest people live in this city',
-      'The area is: 1400 km²',
-      'Population: 360,000 inhabitants',
-      'Number of buildings: 310.000',
-      'The mayors name is: Radmir Lvovich Shchedrin',
-      'The attraction of the city is: first colorful area and first huge park'];
-
-    return this.getRandomArrElem(description);
+    return this.getRandomArrElem(DESCRIPTION);
   }
 
   /** *
    * Generates ad checkin / checkout time
    */
   getAdCheckinOutTime() {
-    const time = ['12:00', '13:00', '14:00'];
-    return this.getRandomArrElem(time);
+    return this.getRandomArrElem(TIME);
   }
 
-  getRandomArr (array, maxLength) {
+  getRandomArr(array, maxLength) {
     return [...Array(1 + Math.random() * maxLength | 0)].map(() => array[Math.random() * array.length | 0]);
   }
 
-  getRandomIndex (array) {
+  getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
   }
 
@@ -164,8 +172,8 @@ class AdDataBuilder {
   getLocaionsValue() {
     return {
       location: {
-        lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
-        lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
+        lat: getRandomPositiveFloat(LAT_RANGE.min, LAT_RANGE.max, 5),
+        lng: getRandomPositiveFloat(LNG_RANGE.min, LNG_RANGE.max, 5),
       },
     };
   }
