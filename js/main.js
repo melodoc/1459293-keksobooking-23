6638448f7fb5b96c0b244a49a0212c35c65c61d6
melodoc <liubov_rz@yahoo.com>
2021-07-13 20:32:32 +0300

    Fix func names

---
+++ js/main.js
@@ -0,0 +1,173 @@
+// import { getRandomPositiveInteger } from './utils/get-random-positive-integer';
+
+
+function getRandomPositiveInteger(first, second) {
+  const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
+  const upper = Math.floor(Math.max(Math.abs(first), Math.abs(second)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+
+function getRandomPositiveFloat (first, second, digits = 1) {
+  const lower = Math.min(Math.abs(first), Math.abs(second));
+  const upper = Math.max(Math.abs(first), Math.abs(second));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+}
+
+const FILE_PATH = 'img/avatars/user';
+const FILE_EXTENSION = '.png';
+
+class AdDataBuilder {
+  constructor(similarAdDescription = [], NUMBER_OF_ADS_NEARBY = 10) {
+    this.similarAdDescription = similarAdDescription;
+    this.NUMBER_OF_ADS_NEARBY = NUMBER_OF_ADS_NEARBY;
+  }
+
+  build() {
+    for (let index = 1; index <= this.NUMBER_OF_ADS_NEARBY; index++) {
+      this.similarAdDescription.push({
+        author: this.getAuthorsValue(index),
+        offer: this.getOffersValue(),
+        locaion: this.getLocaionsValue(),
+      });
+    }
+
+
+    return this.similarAdDescription;
+  }
+
+  getAuthorsValue(serialNumber) {
+    return {
+      avatar: this.setRandomPath(FILE_PATH, serialNumber, FILE_EXTENSION),
+    };
+  }
+
+  /** *
+   * Prepare points for the lines, scatters
+   * @path file path
+   * @id serial number or unique id
+   * @extension file extension with dot: .png
+   */
+  setRandomPath(path, id, extension) {
+    const EXTREME_TWO_DIGIT_NUMBER = 99;
+    const modifiedId = (`0${  id}`).slice(-2);
+    return id > EXTREME_TWO_DIGIT_NUMBER ? `${path}${id}${extension}` : `${path}${modifiedId}${extension}`;
+  }
+
+  getOffersValue() {
+    return {
+      offer: {
+        title: this.getAdTitle(),
+        address: this.getLocaionsValue(),
+        price: getRandomPositiveInteger(1000, 10000),
+        type: this.setAdType(),
+        rooms: getRandomPositiveInteger(1, 10),
+        guests: getRandomPositiveInteger(1, 10),
+        checkin: this.getAdCheckinOutTime(),
+        checkout: this.getAdCheckinOutTime(),
+        features: this.getAdFeatures(),
+        description: this.getAdDescription(),
+        photos: this.getAdPhotos(),
+      },
+    };
+
+  }
+
+  /** *
+   * Generates ad title
+   */
+  getAdTitle() {
+    const adjectives = ['Lovely', 'Beautiful', 'Huge', 'Family', 'Youth', 'Conceptual'];
+    const nouns = ['Abby apartment', 'Milano Flat', 'Guest-house', 'Hilton bungalow'];
+
+    const randomType = Math.floor(Math.random() * adjectives.length);
+    const randomNoun = Math.floor(Math.random() * nouns.length);
+    const name = `${adjectives[randomType]  } ${  nouns[randomNoun]}`;
+    return name;
+  }
+
+  /** *
+   * Generates ad type
+   */
+  setAdType() {
+    const type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+
+    const randomType = Math.floor(Math.random() * type.length);
+    const name = `${type[randomType]}`;
+    return name;
+  }
+
+
+  /** *
+   * Generates ad features
+   */
+  getAdFeatures() {
+    const type = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+    return [...new Set(this.getRandomArr(type, getRandomPositiveInteger(1, type.length - 1)))];
+  }
+
+  /** *
+   * Generates ad photos
+   */
+  getAdPhotos() {
+    const photos = [ 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+    return this.getRandomArr(photos, getRandomPositiveInteger(1, 100));
+  }
+
+  /** *
+   * Generates ad description
+   */
+  getAdDescription() {
+    const type = [ 'This town is called: Norubburg',
+      'It was built: in the west',
+      'Traditional city food: fish',
+      'The given temperature in the city: + 26 ℃',
+      'It was founded: 2600 years ago',
+      'Date of foundation by days and months: February 2nd',
+      'Feature of the city: the richest people live in this city',
+      'The area is: 1400 km²',
+      'Population: 360,000 inhabitants',
+      'Number of buildings: 310.000',
+      'The mayors name is: Radmir Lvovich Shchedrin',
+      'The attraction of the city is: first colorful area and first huge park'];
+
+    const randomType = Math.floor(Math.random() * type.length);
+    const name = `${type[randomType]}`;
+    return name;
+  }
+
+  /** *
+   * Generates ad checkin / checkout time
+   */
+  getAdCheckinOutTime() {
+    const type = ['12:00', '13:00', '14:00'];
+    const randomType = Math.floor(Math.random() * type.length);
+    const name = `${type[randomType]}`;
+    return name;
+  }
+
+  getRandomArr (source, maxLength) {
+    return [...Array(1 + Math.random() * maxLength | 0)].map(() => source[Math.random() * source.length | 0]);
+  }
+
+  getLocaionsValue() {
+    return {
+      location: {
+        lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
+        lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
+      },
+    };
+  }
+
+}
+
+const adv = new AdDataBuilder();
+const data = adv.build();
+
+// eslint-disable-next-line no-console
+console.info(data);
