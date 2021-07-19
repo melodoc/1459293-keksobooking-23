b357116c9fafa3f3668600ac4723029155f19383
melodoc <liubov_rz@yahoo.com>
2021-07-19 21:21:37 +0300

    Add extra validation

--- js/form-management/validate-form.js
+++ js/form-management/validate-form.js
@@ -1,26 +1,21 @@
-const MAX_PRICE_VALUE = 1000000;
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
+import {
+  MAX_PRICE_VALUE,
+  MIN_TITLE_LENGTH,
+  MAX_TITLE_LENGTH,
+  guestNumberRoomMap,
+  validationText,
+  housesType
+} from './prepared-data.js';
 
 const adForm = document.querySelector('.ad-form');
 const title = adForm.querySelector('#title');
 const price = adForm.querySelector('#price');
 const roomNumber = adForm.querySelector('#room_number');
 const capacity = adForm.querySelector('#capacity');
+const roomType = adForm.querySelector('#type');
+const timeIn = adForm.querySelector('#timein');
+const timeOut = adForm.querySelector('#timeout');
 
-const guestNumberRoomMap = {
-  1: ['1'],
-  2: ['1', '2'],
-  3: ['1', '2', '3'],
-  100: ['0'],
-};
-
-const validationText = {
-  minCharacters: 'Минимальное количество символов:',
-  maxCharacters : 'Превышен лимит допустимых символов на:',
-  maxPrice: 'Максимальная цена за ночь не должна превышать ',
-  roomsCount: 'Выборано некоррекное количество гостей',
-};
 
 const validateTitle = () => {
   const valueLength = title.value.length;
@@ -48,6 +43,23 @@ const validateRoomsInput = () => {
   capacity.reportValidity();
 };
 
+const processingOfRoomsInputValue = (targetElement) => {
+  for (let index = 0; index < housesType.length; index++) {
+    if (housesType[index].type === targetElement.value) {
+      price.min = housesType[index].price;
+      price.placeholder = housesType[index].price;
+    }
+  }
+};
+
+const getSynchronizationTime = (targetElement) => {
+  if (timeIn.value !== targetElement.value) {
+    timeIn.value = targetElement.value;
+  } else {
+    timeOut.value = targetElement.value;
+  }
+};
+
 const onChangeHandlerForm = (evt) => {
   switch (evt.target) {
     case title:
@@ -59,6 +71,15 @@ const onChangeHandlerForm = (evt) => {
     case roomNumber:
     case capacity:
       validateRoomsInput();
+      break;
+    case roomType:
+      processingOfRoomsInputValue(roomType);
+      break;
+    case timeIn:
+      getSynchronizationTime(timeIn);
+      break;
+    case timeOut:
+      getSynchronizationTime(timeOut);
       break;
   }
 };
