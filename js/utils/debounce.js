01fd8991cc5be20158042fe5bdb6c9173aa99b53
melodoc <liubov_rz@yahoo.com>
2021-07-21 21:47:45 +0300

    Fix D5

--- js/utils/debounce.js
+++ js/utils/debounce.js
@@ -1,7 +1,10 @@
 // Функция взята из интернета и доработана
 // Источник - https://www.freecodecamp.org/news/javascript-debounce-example
 
-function debounce (callback, timeoutDelay = 500) {
+//   // то есть надо function ...(...){ заменить на const ... = (...) => {
+//  const ... = (...) => {
+
+export const debounce = (callback, timeoutDelay = 500) => {
   // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
   // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
   let timeoutId;
@@ -17,6 +20,4 @@ function debounce (callback, timeoutDelay = 500) {
     // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
     // пока действие совершается чаще, чем переданная задержка timeoutDelay
   };
-}
-
-export {debounce};
+};
